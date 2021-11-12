961695dba87959f549d6675b023f83eb4dd085e0
MOKHTARIA BENAOUALI <benaoualimokhtaria@hotmail.com>
2021-11-12 20:28:50 +0100

    Interface login creation

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             <form method="post" action="index.php">
                 <h1>Login in</h1>
                Login:<span>*</span>
-                <input type=text name="name" placeholder="Your Identifiant!">
+                <input type=text name="name" placeholder="Your Identifiant!!">
                 <br><br>
                 Password:<span>*</span>
                 <input type="password" name="password" placeholder="Your password!">
