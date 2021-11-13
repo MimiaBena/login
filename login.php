7099017d6b1a7e06b9890d9060ec824cc0ec942c
MOKHTARIA BENAOUALI <benaoualimokhtaria@hotmail.com>
2021-11-13 01:44:49 +0100

    update code

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
      require("connexionBd.php");
         session_start();
       //define variable
-     
+     $email=$password= "";
       $messageErr = "";
      if($_SERVER["REQUEST_METHOD"] == "POST"){
       if(isset($_POST['email']) && isset($_POST['password'])){
@@ -14,13 +14,20 @@
                  
             
              
-             $sql ="SELECT count(*) FROM 'users' WHERE email=$email AND password=$password";
-              $result = mysqli_query($con, $sql);
+             $sql ="SELECT * FROM `users` WHERE email='$email' AND password='$password'";
+             $result = mysqli_query($con,$sql) or die('Error: ' . mysqli_error($con));
+  $rows = mysqli_num_rows($result);
+  if($rows==1){
+                 
+                 //$result=execute(array($email, $password));
+                
             //$count = mysqli_num_rows($result);
-      
+     // $reponse      = mysqli_fetch_array($result);
+        //$count = $reponse['count(*)'];
+        //if($result){
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
-      if($result == 0) {
+      //if($result == 1) {
           
         
                       $_SESSION['email'] = $email;
